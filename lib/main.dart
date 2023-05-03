a6f2d208f289cea7b8ea35188e30d76528faa215
GustavWalt <gustavwalterse@gmail.com>
2023-05-03 15:02:34 +0200

    Start movie page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:pvt_23/providers/navigation_bar_provider.dart';
 import 'package:pvt_23/screens/calendarPage/calendar_page.dart';
+import 'package:pvt_23/screens/chosenMoviePage/chosen_movie_page.dart';
 import 'package:pvt_23/screens/homePage/home_page.dart';
 import 'package:pvt_23/screens/loginPage/login_page.dart';
 import 'package:pvt_23/screens/registerPage/register_page.dart';
@@ -29,7 +30,7 @@ final GoRouter _router = GoRouter(
     GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
-          return const CalendarPage();
+          return const ChosenMoviePage();
         }),
     GoRoute(
         path: '/login_page',
