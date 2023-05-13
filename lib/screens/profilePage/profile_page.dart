8da7b48349ce91da8ca9e01839e7594fe4348481
Alliesten <simon.marelius@gmail.com>
2023-05-13 19:31:35 +0200

    red banner centered stack pfp

--- lib/screens/profilePage/profile_page.dart
+++ lib/screens/profilePage/profile_page.dart
@@ -44,7 +44,7 @@ class _ProfilePageState extends State<ProfilePage> {
           ],
         ),
         body: Padding(
-            padding: const EdgeInsets.all(25),
+            padding: const EdgeInsets.all(0),
             child: Column(children: [
               _items.isNotEmpty
                   ? Expanded(
@@ -52,62 +52,63 @@ class _ProfilePageState extends State<ProfilePage> {
                         itemCount: _items.length,
                         itemBuilder: (context, index) {
                           return Column(children: [
-                            Row(
-                              children: [
-                                const Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: CircleAvatar(
-                                    radius: 40,
-                                    backgroundColor: Colors.black,
-                                    child: CircleAvatar(
-                                      radius: 38,
-                                      backgroundImage:
-                                          AssetImage("assets/images/logo1.png"),
+                            Stack(children: <Widget>[
+                              Container(
+                                  height: 90,
+                                  width: double.infinity,
+                                  color:
+                                      const Color.fromARGB(255, 147, 48, 48)),
+                              const Positioned(
+                                  top: 30,
+                                  left: 0,
+                                  right: 0,
+                                  child: Align(
+                                      alignment: Alignment.center,
+                                      child: CircleAvatar(
+                                        radius: 60,
+                                        backgroundColor:
+                                            Color.fromARGB(255, 189, 194, 197),
+                                        child: CircleAvatar(
+                                          radius: 57,
+                                          backgroundImage: AssetImage(
+                                              "assets/images/profile1.png"),
+                                        ),
+                                      ))),
+                              Card(
+                                margin:
+                                    const EdgeInsets.fromLTRB(30, 130, 30, 8),
+                                color: Colors.transparent,
+                                elevation: 0,
+                                child: Column(children: <Widget>[
+                                  const ListTile(
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 0),
+                                    title: Text('Username',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        )),
+                                  ),
+                                  TextField(
+                                    decoration: InputDecoration(
+                                      contentPadding:
+                                          const EdgeInsets.symmetric(
+                                              horizontal: 15.0),
+                                      filled: true,
+                                      fillColor: const Color.fromARGB(
+                                          255, 189, 194, 197),
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      hintText: 'JaneDoe1',
                                     ),
                                   ),
-                                ),
-                                Text(
-                                  _items[index]["name"],
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20),
-                                )
-                              ],
-                            ),
+                                ]),
+                              ),
+                            ]),
                             Card(
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 10),
-                              color: const Color.fromARGB(255, 35, 33, 26),
-                              elevation: 0,
-                              child: Column(children: <Widget>[
-                                const ListTile(
-                                  contentPadding:
-                                      EdgeInsets.symmetric(horizontal: 0),
-                                  title: Text('Username',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                      )),
-                                ),
-                                TextField(
-                                  decoration: InputDecoration(
-                                    contentPadding: const EdgeInsets.symmetric(
-                                        horizontal: 15.0),
-                                    filled: true,
-                                    fillColor: const Color.fromARGB(
-                                        255, 189, 194, 197),
-                                    border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10)),
-                                    hintText: 'JaneDoe1',
-                                  ),
-                                ),
-                              ]),
-                            ),
-                            Card(
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 10),
-                              color: const Color.fromARGB(255, 35, 33, 26),
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 8),
+                              color: Colors.transparent,
                               elevation: 0,
                               child: Column(children: <Widget>[
                                 const ListTile(
@@ -135,8 +136,8 @@ class _ProfilePageState extends State<ProfilePage> {
                             ),
                             Card(
                               margin: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 0),
-                              color: const Color.fromARGB(255, 35, 33, 26),
+                                  horizontal: 30, vertical: 8),
+                              color: Colors.transparent,
                               elevation: 0,
                               child: Column(children: <Widget>[
                                 const ListTile(
@@ -163,9 +164,9 @@ class _ProfilePageState extends State<ProfilePage> {
                               ]),
                             ),
                             Card(
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 10),
-                              color: const Color.fromARGB(255, 35, 33, 26),
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 30, vertical: 8),
+                              color: Colors.transparent,
                               elevation: 0,
                               child: Column(children: <Widget>[
                                 const ListTile(
@@ -192,8 +193,8 @@ class _ProfilePageState extends State<ProfilePage> {
                               ]),
                             ),
                             const Padding(
-                                padding: EdgeInsets.all(20),
-                                child: Text("Uppdatera",
+                                padding: EdgeInsets.all(18),
+                                child: Text("Update",
                                     style: TextStyle(
                                       fontSize: 18,
                                       fontWeight: FontWeight.bold,
@@ -205,7 +206,7 @@ class _ProfilePageState extends State<ProfilePage> {
                     )
                   : Container(),
               Container(
-                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                         minimumSize: const Size.fromRadius(10),
