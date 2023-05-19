185d7e3b4636f39b76f5c1135e63258943d3586b
SimonThern <sirret123@gmail.com>
2023-05-19 09:13:54 +0200

    added old files

---
+++ test/find_group_result_page_test.dart
@@ -0,0 +1,10 @@
+import 'package:pvt_23/screens/FindGroupResultPage/find_group_result_page.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Groups', () {
+    test('Join group' , () {
+
+    });
+  });
+}
