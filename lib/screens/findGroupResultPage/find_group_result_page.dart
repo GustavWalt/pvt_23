38881dfb2f27ddc3e13d71294961127c616e4f01
minnaq <minnaq@kth.se>
2023-05-27 08:18:20 +0200

    no groups found message when searching

--- lib/screens/findGroupResultPage/find_group_result_page.dart
+++ lib/screens/findGroupResultPage/find_group_result_page.dart
@@ -32,177 +32,219 @@ class _FindGroupResultPageState extends State<FindGroupResultPage> {
           return Text("Loading");
         }
 
-        return Scaffold(
-          bottomNavigationBar: const MenuWidget(),
-          backgroundColor: Color.fromARGB(255, 35, 33, 26),
-          appBar: AppBar(
-            actions: [
-              Padding(
-                  padding: EdgeInsets.only(right: 20),
-                  child: GestureDetector(
-                    onTap: () {
-                      context.go("/profile_page");
-                    },
-                    child: Icon(Icons.account_circle),
-                  ))
-            ],
-            leading: Builder(
-              builder: (BuildContext context) {
-                return IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () {
-                    context.go("/find_new_group_page");
+        if (snapshot.data?.docs.length == 0) {
+          print("hej");
+          return Scaffold(
+              bottomNavigationBar: const MenuWidget(),
+              backgroundColor: Color.fromARGB(255, 35, 33, 26),
+              appBar: AppBar(
+                actions: [
+                  Padding(
+                      padding: EdgeInsets.only(right: 20),
+                      child: GestureDetector(
+                        onTap: () {
+                          context.go("/profile_page");
+                        },
+                        child: Icon(Icons.account_circle),
+                      ))
+                ],
+                leading: Builder(
+                  builder: (BuildContext context) {
+                    return IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () {
+                        context.go("/find_new_group_page");
+                      },
+                    );
                   },
-                );
-              },
-            ),
-            title: const Text('Found groups'),
-            backgroundColor: Colors.black,
-            centerTitle: true,
-          ),
-          body: ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data()! as Map<String, dynamic>;
-              String docId = document.id;
-              return Padding(
-                padding: EdgeInsets.all(20),
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  color: const Color.fromARGB(255, 147, 48, 48),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        title: Text(data['name'],
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                                color: Colors.white)),
-                        subtitle: Text(data['description'],
-                            style: const TextStyle(color: Colors.white)),
-                      ),
-                      Container(
-                          alignment: Alignment.bottomLeft,
-                          padding: EdgeInsets.fromLTRB(16, 0, 0, 5),
-                          child: Text("${"Genre: " + data['genre'].toString()}",
-                              style: const TextStyle(color: Colors.white))),
-                      Container(
-                          alignment: Alignment.bottomLeft,
-                          padding: EdgeInsets.fromLTRB(16, 0, 0, 5),
-                          child: Text(
-                              "${"Discussion level: " + data['level'].toString()}",
-                              style: const TextStyle(color: Colors.white))),
-                      Container(
-                          alignment: Alignment.bottomLeft,
-                          padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
-                          child: Text(
-                              "${"Members: " + data['members'].toString() + "/" + data['size'].toString()}",
-                              style: const TextStyle(color: Colors.white))),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          TextButton(
-                            child: const Text('Join',
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.white)),
-                            onPressed: () async {
-                              final FirebaseAuth auth = FirebaseAuth.instance;
-                              final User? currentUser = auth.currentUser;
-                              final uid = currentUser!.uid;
-
-                              List _items = [];
-                              Map<String, dynamic> groups2 = {};
-                              bool userIsInGroup = false;
-
-                              var db2 = FirebaseFirestore.instance
-                                  .collection("users")
-                                  .doc(auth.currentUser!.uid);
-
-                              await db2
-                                  .get()
-                                  .then((value) => groups2 = value["groups"]);
-                              groups2.forEach((key, value) {
-                                _items = value;
-                              });
-
-                              for (var i = 0; i < _items.length; i++) {
-                                if (_items[i] == docId) {
-                                  userIsInGroup = true;
-                                }
-                              }
-
-                              if (userIsInGroup) {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: const Text("Error"),
-                                      content: const Text(
-                                          "You are already part of that group!"),
-                                      actions: [
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: const Text("OK"),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                                return;
-                              } else if (data['members'] >=
-                                  int.parse(data['size'])) {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: const Text("Error"),
-                                      content: const Text(
-                                          "This group is already full!"),
-                                      actions: [
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: const Text("OK"),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                                return;
-                              } else {
-                                await db
-                                    .collection("users")
-                                    .doc(auth.currentUser!.uid)
-                                    .update({
-                                  'groups.id': FieldValue.arrayUnion([docId]),
-                                });
-
-                                await db
-                                    .collection("groups")
-                                    .doc(docId)
-                                    .update({
-                                  "members": FieldValue.increment(1),
-                                });
-                                context.go('/group_page');
-                              }
-                              userIsInGroup = false;
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
                 ),
-              );
-            }).toList(),
-          ),
-        );
+                title: const Text('Found groups'),
+                backgroundColor: Colors.black,
+                centerTitle: true,
+              ),
+              body: Center(
+                  child: Text(
+                "No groups were found that match your search.",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              )));
+        } else {
+          return Scaffold(
+            bottomNavigationBar: const MenuWidget(),
+            backgroundColor: Color.fromARGB(255, 35, 33, 26),
+            appBar: AppBar(
+              actions: [
+                Padding(
+                    padding: EdgeInsets.only(right: 20),
+                    child: GestureDetector(
+                      onTap: () {
+                        context.go("/profile_page");
+                      },
+                      child: Icon(Icons.account_circle),
+                    ))
+              ],
+              leading: Builder(
+                builder: (BuildContext context) {
+                  return IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      context.go("/find_new_group_page");
+                    },
+                  );
+                },
+              ),
+              title: const Text('Found groups'),
+              backgroundColor: Colors.black,
+              centerTitle: true,
+            ),
+            body: ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                String docId = document.id;
+                return Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    color: const Color.fromARGB(255, 147, 48, 48),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(data['name'],
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.white)),
+                          subtitle: Text(data['description'],
+                              style: const TextStyle(color: Colors.white)),
+                        ),
+                        Container(
+                            alignment: Alignment.bottomLeft,
+                            padding: EdgeInsets.fromLTRB(16, 0, 0, 5),
+                            child: Text(
+                                "${"Genre: " + data['genre'].toString()}",
+                                style: const TextStyle(color: Colors.white))),
+                        Container(
+                            alignment: Alignment.bottomLeft,
+                            padding: EdgeInsets.fromLTRB(16, 0, 0, 5),
+                            child: Text(
+                                "${"Discussion level: " + data['level'].toString()}",
+                                style: const TextStyle(color: Colors.white))),
+                        Container(
+                            alignment: Alignment.bottomLeft,
+                            padding: EdgeInsets.fromLTRB(16, 0, 0, 0),
+                            child: Text(
+                                "${"Members: " + data['members'].toString() + "/" + data['size'].toString()}",
+                                style: const TextStyle(color: Colors.white))),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            TextButton(
+                              child: const Text('Join',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white)),
+                              onPressed: () async {
+                                final FirebaseAuth auth = FirebaseAuth.instance;
+                                final User? currentUser = auth.currentUser;
+                                final uid = currentUser!.uid;
+
+                                List _items = [];
+                                Map<String, dynamic> groups2 = {};
+                                bool userIsInGroup = false;
+
+                                var db2 = FirebaseFirestore.instance
+                                    .collection("users")
+                                    .doc(auth.currentUser!.uid);
+
+                                await db2
+                                    .get()
+                                    .then((value) => groups2 = value["groups"]);
+                                groups2.forEach((key, value) {
+                                  _items = value;
+                                });
+
+                                for (var i = 0; i < _items.length; i++) {
+                                  if (_items[i] == docId) {
+                                    userIsInGroup = true;
+                                  }
+                                }
+
+                                if (userIsInGroup) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text("Error"),
+                                        content: const Text(
+                                            "You are already part of that group!"),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text("OK"),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                  return;
+                                } else if (data['members'] >=
+                                    int.parse(data['size'])) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text("Error"),
+                                        content: const Text(
+                                            "This group is already full!"),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text("OK"),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                  return;
+                                } else {
+                                  await db
+                                      .collection("users")
+                                      .doc(auth.currentUser!.uid)
+                                      .update({
+                                    'groups.id': FieldValue.arrayUnion([docId]),
+                                  });
+
+                                  await db
+                                      .collection("groups")
+                                      .doc(docId)
+                                      .update({
+                                    "members": FieldValue.increment(1),
+                                  });
+                                  context.go('/group_page');
+                                }
+                                userIsInGroup = false;
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          );
+        }
       },
     );
   }
